b6123e3e2bcc0da9614eba78a6e995eede311650
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-11-10 09:50:53 -0300

    HEY-9: it should check if ends with question mark?

--- tests/Feature/CreateAQuestionTest.php
+++ tests/Feature/CreateAQuestionTest.php
@@ -27,20 +27,35 @@ it('should be able to create a new question bigger than 255 characters', functio
 });
 
 it('should check if ends with question mark?', function () {
-    expect(true)->toBeTruthy();
+    // Arrange :: preparar
+    $user = User::factory()->create();
+    actingAs($user);
+    // Act :: agir
+    $request = post(route('question.store'), [
+        'question' => str_repeat('*', 10),
+    ]);
+    // Assert :: verificar
+    $request->assertSessionHasErrors([
+        'question' => 'Are you sure that is a question? It is missing the question mark in the end.',
+    ]);
+
+    assertDatabaseCount('questions', 0);
 });
 
 it('should have at least 10 characters', function () {
     // Arrange :: preparar
     $user = User::factory()->create();
     actingAs($user);
-
     // Act :: agir
     $request = post(route('question.store'), [
         'question' => str_repeat('*', 8).'?',
     ]);
-
     // Assert :: verificar
-    $request->assertSessionHasErrors(['question' => __('validation.min.string', ['min' => 10, 'attribute' => 'question'])]);
+    $request->assertSessionHasErrors([
+        'question' => __('validation.min.string', [
+            'min'       => 10,
+            'attribute' => 'question',
+        ]),
+    ]);
     assertDatabaseCount('questions', 0);
 });
