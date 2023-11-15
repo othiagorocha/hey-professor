b03a11ff18e2345df2607e9aeb937e7432f2fa26
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-11-15 16:16:43 -0300

    hey-14: it should be able to destroy a question

---
+++ resources/views/components/table/th.blade.php
@@ -0,0 +1,6 @@
+<th
+  scope="col"
+  class="px-6 py-3"
+>
+  {{ $slot }}
+</th>
--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -19,15 +19,83 @@
 
     <hr class="border-gray-700 my-4 border-dashed">
 
-    {{-- listagem --}}
-    <div class="dark:text-gray-300 uppercase mb-1 font-bold">
+    {{-- Drafts --}}
+    <div class="dark:text-gray-300 uppercase mb-3 font-bold">
+      My Drafts
+
+
+    </div>
+
+
+    <div class="dark:text-gray-400 space-y-4">
+      <x-table>
+        <x-table.thead>
+          <tr>
+            <x-table.th>Questions</x-table.th>
+            <x-table.th>Actions</x-table.th>
+          </tr>
+        </x-table.thead>
+
+        <tbody>
+          @foreach ($questions->where('draft', true) as $question)
+            <x-table.tr>
+              <x-table.td>{{ $question->question }}</x-table.td>
+              <x-table.td>
+                <x-form
+                  :action="route('question.publish', $question)"
+                  put
+                >
+                  <button
+                    type="submit"
+                    class="hover:underline text-blue-500"
+                  >
+                    Publish
+                  </button>
+                </x-form>
+                <x-form
+                  :action="route('question.destroy', $question)"
+                  delete
+                >
+                  <button
+                    type="submit"
+                    class="hover:underline text-red-500"
+                  >
+                    >
+                    Delete
+                  </button>
+                </x-form>
+
+              </x-table.td>
+            </x-table.tr>
+          @endforeach
+        </tbody>
+      </x-table>
+    </div>
+    <hr class="border-gray-700 my-4 border-dashed">
+
+    {{-- Questions --}}
+    <div class="dark:text-gray-300 uppercase mb-3 font-bold">
       My Questions
     </div>
 
     <div class="dark:text-gray-400 space-y-4">
-      @foreach ($questions as $item)
-        <x-question :question="$item" />
-      @endforeach
+      <x-table>
+        <x-table.thead>
+          <tr>
+            <x-table.th>Questions</x-table.th>
+            <x-table.th>Actions</x-table.th>
+          </tr>
+        </x-table.thead>
+
+        <tbody>
+          @foreach ($questions->where('draft', false) as $question)
+            <x-table.tr>
+              <x-table.td>{{ $question->question }}</x-table.td>
+              <x-table.td>[ deletar ] [ publicar ]</x-table.td>
+            </x-table.tr>
+          @endforeach
+        </tbody>
+      </x-table>
     </div>
 
   </x-container>
