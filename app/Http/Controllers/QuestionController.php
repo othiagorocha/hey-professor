b2cd546dbeb8197a54aefec3fb49808733e3d49f
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-11-16 11:37:56 -0300

    hey-16: it should make sure that only question with status DRAFT and only if the question was created by the logged user can be updated

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -44,6 +44,7 @@ class QuestionController extends Controller
     }
     public function update(Question $question): RedirectResponse
     {
+        $this->authorize('update', $question);
         $question->question = request()->question;
         $question->save();
 
--- tests/Feature/Question/UpdateTest.php
+++ tests/Feature/Question/UpdateTest.php
@@ -20,3 +20,30 @@ it('should be able to update a question', function () {
 
     expect($question)->question->toBe('Is a updated question?');
 });
+
+it('should make sure that only question with status DRAFT can be updated', function () {
+    $user             = User::factory()->create();
+    $questionNotDraft = Question::factory()->for($user, 'createdBy')->create(['draft' => false]);
+    $draftQuestion    = Question::factory()->for($user, 'createdBy')->create(['draft' => true]);
+
+    actingAs($user);
+
+    put(route('question.update', $questionNotDraft))->assertForbidden();
+    put(route('question.update', $draftQuestion), ['question' => 'Update question without "?"?'])->assertRedirect();
+});
+
+it('should make sure that only the person who has created the question can update the question', function () {
+    $rightUser = User::factory()->create();
+    $wrongUser = User::factory()->create();
+    $question  = Question::factory()->create(['draft' => true, 'created_by' => $rightUser->id]);
+
+    actingAs($wrongUser);
+    put(route('question.update', $question))
+      ->assertForbidden()
+    ;
+
+    actingAs($rightUser);
+    put(route('question.update', $question), ['question' => 'Update question without "?"?'])
+      ->assertRedirect()
+    ;
+});
