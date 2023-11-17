df922c52184755a9eb8bc951807d15ab84e08cb7
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-11-17 15:19:23 -0300

    hey-18: ordering based on like/unlikes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,7 +11,12 @@ class DashboardController extends Controller
     {
         return view('dashboard', [
             'questions' => Question::withSum('votes', 'like')
-              ->withSum('votes', 'unlike')->paginate(5),
+              ->withSum('votes', 'unlike')
+              ->orderByRaw('
+                case when votes_sum_like is null then 0 else votes_sum_like end desc,
+                case when votes_sum_unlike is null then 0 else votes_sum_unlike end
+          ')
+              ->paginate(5),
         ]);
     }
 }
--- tests/Feature/Question/ListTest.php
+++ tests/Feature/Question/ListTest.php
@@ -24,11 +24,33 @@ it('should list all the questions', function () {
 });
 
 it('should paginate the result', function () {
-    $user      = User::factory()->create();
-    $questions = Question::factory()->count(20)->create();
-
+    $user = User::factory()->create();
+    Question::factory()->count(20)->create();
     actingAs($user);
     get(route('dashboard'))
       ->assertViewHas('questions', fn ($value) => $value instanceof LengthAwarePaginator)
+    ;
+});
+
+it('should order by like and unlike, most liked should be at the top, most unliked questions should be in the bottom', function () {
+    $user       = User::factory()->create();
+    $secondUser = User::factory()->create();
+    Question::factory()->count(5)->create();
+    $mostLikedQuestion   = Question::find(3);
+    $mostUnlikedQuestion = Question::find(1);
+    $user->like($mostLikedQuestion);
+    $secondUser->unlike($mostUnlikedQuestion);
+
+    actingAs($user);
+    get(route('dashboard'))
+      ->assertViewHas('questions', function ($questions) {
+          expect($questions)
+            ->first()->id->toBe(3)
+            ->and($questions)
+            ->last()->id->toBe(1)
+          ;
+
+          return true;
+      })
     ;
 });
