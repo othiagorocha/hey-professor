f07835f676e16e1b3a9e13b0fa9b610dc7bebb64
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-11-20 11:14:44 -0300

    hey-24: wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,38 +7,40 @@
 
   <x-container>
     {{-- listagem --}}
-    <div class="relative mb-4 flex justify-between">
+    <div class="relative mb-4 flex flex-col justify-between">
       <p class="mb-1 font-bold uppercase dark:text-gray-300">
         List of questions
       </p>
 
       <form
-        class="absolute -right-2 -top-4 flex w-fit items-start gap-3 p-0"
+        class="-right-2 -top-4 flex w-full flex-col items-start gap-3 p-0 sm:w-fit sm:flex-row md:absolute"
         action="{{ route('dashboard') }}"
         method="get">
         @csrf
         <x-text-input
-          class="h-fit w-[18.75rem] rounded-md py-1.5"
+          class="h-fit w-full rounded-md py-1.5 sm:w-[18.75rem]"
           type="text"
           name="search"
           value="{{ request()->search }}" />
-        <x-btn.primary class="py-2"
-          type="submit">Search</x-btn.primary>
+        <x-btn.primary class="w-full py-2 sm:w-fit"
+          type="submit">Search
+        </x-btn.primary>
       </form>
     </div>
 
     <div class="space-y-4 dark:text-gray-400">
 
       @if ($questions->isEmpty())
-        <div class="flex flex-col items-center justify-center gap-10">
+        <div
+          class="flex flex-col items-start justify-center gap-10 sm:items-center">
           <div>
-            <x-draw.searching width="350" />
+            <x-draw.searching class="w-[220px] md:w-[350px]" />
           </div>
-          <div>
-            <div class="text-2xl font-bold dark:text-gray-400">
+          <div class="flex w-full justify-center">
+            <p class="font-bold dark:text-gray-400 sm:text-2xl">
               Question
               not found
-            </div>
+            </p>
           </div>
         </div>
       @else
