6922ec2d96c2a67f0193e182d062d01f0844f65f
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-11-20 10:48:59 -0300

    hey-24: applying filter in the front end

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,14 +7,45 @@
 
   <x-container>
     {{-- listagem --}}
+    <div class="relative mb-4 flex justify-between">
+      <p class="mb-1 font-bold uppercase dark:text-gray-300">
+        List of questions
+      </p>
 
-    <div class="mb-1 font-bold uppercase dark:text-gray-300">List of
-      questions</div>
+      <form
+        class="absolute -right-2 -top-4 flex w-fit items-start gap-3 p-0"
+        action="{{ route('dashboard') }}"
+        method="get">
+        @csrf
+        <x-text-input
+          class="h-fit w-[18.75rem] rounded-md py-1.5"
+          type="text"
+          name="search"
+          value="{{ request()->search }}" />
+        <x-btn.primary class="py-2"
+          type="submit">Search</x-btn.primary>
+      </form>
+    </div>
 
     <div class="space-y-4 dark:text-gray-400">
-      @foreach ($questions as $item)
-        <x-question :question="$item" />
-      @endforeach
+
+      @if ($questions->isEmpty())
+        <div class="flex flex-col items-center justify-center gap-10">
+          <div>
+            <x-draw.searching width="350" />
+          </div>
+          <div>
+            <div class="text-2xl font-bold dark:text-gray-400">
+              Question
+              not found
+            </div>
+          </div>
+        </div>
+      @else
+        @foreach ($questions as $item)
+          <x-question :question="$item" />
+        @endforeach
+      @endif
 
       {{ $questions->withQueryString()->links() }}
     </div>
